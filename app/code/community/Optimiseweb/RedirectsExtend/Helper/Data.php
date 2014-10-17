e0c6de8465871fb14bc36b910c55ac937483605e
Kathirvel <sid@kathirvel.com>
2014-10-17 16:09:08 +0100

    Initial Push

---
+++ app/code/community/Optimiseweb/RedirectsExtend/Helper/Data.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Optimiseweb RedirectsExtend Data Helper
+ *
+ * @package     Optimiseweb_RedirectsExtend
+ * @author      Kathir Vel (sid@optimiseweb.co.uk)
+ * @copyright   Copyright (c) 2014 Optimise Web
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Optimiseweb_RedirectsExtend_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    
+}
---
+++ app/code/community/Optimiseweb/RedirectsExtend/Model/Observer.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Optimiseweb RedirectsExtend Model Redirector
+ *
+ * @package     Optimiseweb_RedirectsExtend
+ * @author      Kathir Vel (sid@optimiseweb.co.uk)
+ * @copyright   Copyright (c) 2014 Optimise Web
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Optimiseweb_RedirectsExtend_Model_Observer extends Varien_Object
+{
+
+    /**
+     * Run before the Legacy version of redirection is executed
+     * 
+     * @param type $evt
+     */
+    public function beforeLegacy($evt)
+    {
+        $event = $evt->getEvent();
+        $requestUrl = $event->getRequestUrl();
+    }
+
+    /**
+     * Run before the Version One of redirection is executed
+     * 
+     * @param type $evt
+     */
+    public function beforeVersionOne($evt)
+    {
+        $event = $evt->getEvent();
+        $requestUrl = $event->getRequestUrl();
+    }
+
+    /**
+     * Run before the Query Strings version of redirection is executed
+     * 
+     * @param type $evt
+     */
+    public function beforeQueryStrings($evt)
+    {
+        $event = $evt->getEvent();
+        $requestUrl = $event->getRequestUrl();
+    }
+
+    /**
+     * Run before the Catalogue Search version of redirection is executed
+     * 
+     * @param type $evt
+     */
+    public function beforeCatalogueSearch($evt)
+    {
+        $event = $evt->getEvent();
+        $requestUrl = $event->getRequestUrl();
+    }
+
+    /**
+     * Run before the module gives up and router flow exits to display a 404 page
+     * 
+     * @param type $evt
+     */
+    public function beforeExiting($evt)
+    {
+        $event = $evt->getEvent();
+        $requestUrl = $event->getRequestUrl();
+    }
+
+}
